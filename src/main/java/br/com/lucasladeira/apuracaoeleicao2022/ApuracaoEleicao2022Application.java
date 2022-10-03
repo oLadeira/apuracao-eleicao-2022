f4fdd9a353eda3d4d0f97a9f2e1ec45815a71454
Lucas Ladeira <lucas-ladeira@hotmail.com>
2022-10-03 09:44:23 -0300

    Initial commit

---
+++ src/main/java/br/com/lucasladeira/apuracaoeleicao2022/ApuracaoEleicao2022Application.java
@@ -0,0 +1,13 @@
+package br.com.lucasladeira.apuracaoeleicao2022;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApuracaoEleicao2022Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApuracaoEleicao2022Application.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/lucasladeira/apuracaoeleicao2022/ApuracaoEleicao2022ApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lucasladeira.apuracaoeleicao2022;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApuracaoEleicao2022ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
